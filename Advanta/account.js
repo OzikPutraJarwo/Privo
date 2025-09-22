4759d28f0a8a48ed28b8c34d9f8f92ed1fa507a4
Ozik Jarwo <jarwoozik08@gmail.com>
2025-09-22 15:35:03 +0900

    Advanta - Location added

--- Advanta/account.js
+++ Advanta/account.js
@@ -147,6 +147,7 @@ async function ensureInventoryFiles() {
   cropFileId = await createOrGetFile('crop.json', subFolders.inventory, { crops: [] });
   lineFileId = await createOrGetFile('line.json', subFolders.inventory, { lines: [] });
   paramFileId = await createOrGetFile('param.json', subFolders.inventory, { params: [] });
+  locationFileId = await createOrGetFile('location.json', subFolders.inventory, { locations: [] });
 }
 
 async function createOrGetFile(name, parentId, defaultJson) {
@@ -884,6 +885,130 @@ function addParamValue(e) {
 function deleteRange(btn) { 
   const tr = btn.closest('.tr');
   tr.remove();
+}
+
+
+//////// Locations
+
+
+let locationFileId = null;
+let locationData = [];
+
+async function loadLocationData() {
+  await ensureToken();
+  if (!locationFileId) {
+    const q = `name='location.json' and '${subFolders.inventory}' in parents and trashed=false`;
+    const res = await gapi.client.drive.files.list({ q, fields: 'files(id, name)' });
+    if (res.result.files && res.result.files.length) {
+      locationFileId = res.result.files[0].id;
+    } else {
+      locationFileId = await createOrGetFile('location.json', subFolders.inventory, { locations: [] });
+    }
+  }
+
+  const accessToken = gapi.client.getToken().access_token;
+  const resp = await fetch(`https://www.googleapis.com/drive/v3/files/${locationFileId}?alt=media`, {
+    headers: { Authorization: `Bearer ${accessToken}` }
+  });
+
+  if (!resp.ok) {
+    console.error('Gagal memuat location.json:', await resp.text());
+    locationData = [];
+    return;
+  }
+
+  const json = await resp.json();
+  locationData = Array.isArray(json.locations) ? json.locations : [];
+
+  if (typeof renderLocationTable === 'function') {
+    renderLocationTable();
+  }
+}
+
+async function saveLocation() {
+  const id = document.getElementById("editLocationId").value;
+  const locationName = document.getElementById("locationName").value;
+
+  if (id) {
+    const idx = locationData.findIndex(l => l.id === id);
+    if (idx !== -1) {
+      locationData[idx] = { id, locationName };
+    }
+  } else {
+    locationData.push({
+      id: generateId(),
+      locationName
+    });
+  }
+
+  notification("loading", "Saving location...");
+  await updateLocationJson();
+  renderLocationTable();
+  resetLocationForm();
+  closePopup();
+  notification("success", "Location saved");
+}
+
+function renderLocationTable() {
+  const tbody = document.querySelector("#locations .table .tbody");
+  tbody.innerHTML = "";
+  locationData.forEach(location => {
+    const tr = document.createElement("div");
+    tr.classList.add('tr');
+    tr.innerHTML = `
+      <div class="td no center"></div>
+      <div class="td">${location.locationName}</div>
+      <div class="td action">
+        <button id="edit" onclick="editLocation('${location.id}');openPopup('.locations')">
+          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <g id="style=locationar"> <g id="edit"> <path id="vector" d="M18.4101 3.6512L20.5315 5.77252C21.4101 6.6512 21.4101 8.07582 20.5315 8.9545L9.54019 19.9458C9.17774 20.3082 8.70239 20.536 8.19281 20.5915L4.57509 20.9856C3.78097 21.072 3.11061 20.4017 3.1971 19.6076L3.59111 15.9898C3.64661 15.4803 3.87444 15.0049 4.23689 14.6425L3.70656 14.1121L4.23689 14.6425L15.2282 3.6512C16.1068 2.77252 17.5315 2.77252 18.4101 3.6512Z" stroke-width="2"/> <path id="vector_2" d="M15.2282 3.6512C16.1068 2.77252 17.5315 2.77252 18.4101 3.6512L20.5315 5.77252C21.4101 6.6512 21.4101 8.07582 20.5315 8.9545L18.7283 10.7576L13.425 5.45432L15.2282 3.6512Z" stroke-width="2"/> </g> </g> </svg>
+        </button>
+        <button id="delete" onclick="deleteLocation('${location.id}')">
+          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6" stroke-width="2" stroke-locationcap="round" stroke-locationjoin="round"/> </svg>
+        </button>
+      </div>
+    `;
+    tbody.prepend(tr);
+  });
+}
+
+function editLocation(id) {
+  const location = locationData.find(l => l.id === id);
+  if (!location) return;
+
+  document.getElementById("editLocationId").value = location.id;
+  document.getElementById("locationName").value = location.locationName;
+  document.getElementById("saveLocationBtn").textContent = "Save Changes";
+}
+
+function resetLocationForm() {
+  document.getElementById("locationForm").reset();
+  document.getElementById("editLocationId").value = "";
+  document.getElementById("saveLocationBtn").textContent = "Add Location";
+}
+
+async function deleteLocation(id) {
+  notification('loading', 'Deleting location...');
+  locationData = locationData.filter(l => l.id !== id);
+  await updateLocationJson();
+  renderLocationTable();
+  notification('success', 'Location deleted');
+}
+
+async function updateLocationJson() {
+  await ensureToken();
+  const accessToken = gapi.client.getToken().access_token;
+
+  const content = JSON.stringify({ locations: locationData }, null, 2);
+
+  await gapi.client.request({
+    path: `/upload/drive/v3/files/${locationFileId}?uploadType=media`,
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`
+    },
+    body: content
+  });
 }
 
 //////// Additional
